ecc3e7d43e6c4d7ef57d052dbd750cf6c6e120a7
ndev <napodevesa@gmail.com>
2020-08-04 16:02:02 +0200

    fondo.objeto

--- ngame/villas.js
+++ ngame/villas.js
@@ -2,46 +2,34 @@
 var vp = document.getElementById("villas");
 var papel = vp.getContext ("2d");
 
-
 var vaca = { url: "vaca.png", cargaOK: false};
-
 var fondo = { url: "tile.png", cargaOK: false };
-
 
 fondo.objeto = new Image();
 fondo.objeto.src = fondo.url;
 fondo.objeto.addEventListener("load", cargarFondo);
 
-
 vaca.objeto = new Image();
 vaca.objeto.src = vaca.url;
 vaca.objeto.addEventListener("load", cargarVaca);
 
-
 function cargarFondo()
 {
-
 fondo.cargaOk = true;
 dibujar (fondo);
-
 }
 
 function cargarVaca()
 {
-
 vaca.cargaOk = true;
 dibujar (vaca);
-
-
 }
-
 
 function dibujar ()
 {
-
-	if (fondo.cargaOk==true){
-
-		papel.drawImage (fondo.imagen,0,0);
+	if (fondo.cargaOk == true)
+	{
+		papel.drawImage (fondo.objeto,0,0);
 	}
 }
 
@@ -49,11 +37,8 @@ function dibujar ()
 
 function aleatorio (min, max)
 {
-
 	var resultado;
-
-	resultado = Math.floor(Math.random() * (max-min+1))+min;
-
+	resultado = Math.floor (Math.random() * (max-min+1)) + min;
 	return resultado;
 }
 
