3251546a8f2f1e6df98061c4589f0e6be1506888
ndev <napodevesa@gmail.com>
2020-08-04 15:43:26 +0200

    modificaciones gamejs

--- ngame/villas.js
+++ ngame/villas.js
@@ -1,52 +1,51 @@
 
 var vp = document.getElementById("villas");
 var papel = vp.getContext ("2d");
-var mapa = "tile.png"
 
-var fondo = new Image();
-fondo.src = mapa;
-fondo.addEventListener("load", dibujar);
 
-var vaca = new Image();
-vaca.src = "vaca.png";
-vaca.addEventListener("load", dibujarVaca);
+var vaca = { url: "vaca.png", cargaOK: false};
 
-var pollo = new Image();
-pollo.src = "pollo.png";
-pollo.addEventListener("load", dibujarPollo);
+var fondo = { url: "tile.png", cargaOK: false };
 
-var cerdo = new Image();
-cerdo.src = "cerdo.png";
-cerdo.addEventListener("load", dibujarCerdo);
+
+fondo.objeto = new Image();
+fondo.objeto.src = fondo.url;
+fondo.objeto.addEventListener("load", cargarFondo);
+
+
+vaca.objeto = new Image();
+vaca.objeto.src = vaca.url;
+vaca.objeto.addEventListener("load", cargarVaca);
+
+
+function cargarFondo()
+{
+
+fondo.cargaOk = true;
+dibujar (fondo);
+
+}
+
+function cargarVaca()
+{
+
+vaca.cargaOk = true;
+dibujar (vaca);
+
+
+}
 
 
 function dibujar ()
 {
 
-papel.drawImage(fondo,0,0);
+	if (fondo.cargaOk==true){
 
+		papel.drawImage (fondo.imagen,0,0);
+	}
 }
 
-function dibujarVaca ()
-{
 
-papel.drawImage(vaca,10,10);
-
-}
-
-function dibujarPollo ()
-{
-
-papel.drawImage(cerdo,10,20);
-
-}
-
-function dibujarCerdo ()
-{
-
-papel.drawImage(pollo,30,15);
-
-}
 
 
 /*
