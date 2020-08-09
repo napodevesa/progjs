2328e2573788143bbe1912db5818a2773e5575f0
ndev <napodevesa@gmail.com>
2020-08-09 11:17:58 +0200

    cambios dibujo js

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -6,6 +6,7 @@ var texto = document.getElementById("texto_lineas");
 var boton = document.getElementById("botoncito");
 boton.addEventListener("click", dibujoPorClick);
 
+
 var d = document.getElementById("dibujito");
 var ancho = d.width;
 var lienzo = d.getContext("2d") ;
@@ -32,9 +33,11 @@ function dibujoPorClick() {
 
 	for(l=0;l < lineas; l++){
 		dibujarLinea (colorcito , 0, yinicial, xfinal, 300);
+
 		dibujarLinea (colorcito , yinicial, 0, 300, xfinal);
-		dibujarLinea (colorcito , 0, 0, 300, 300);
-		dibujarLinea (colorcito , 0, 300, 300, 0);
+
+		dibujarLinea (colorcito , 300, 0, 0, yinicial);
+		dibujarLinea (colorcito , 300, 300, xfinal, 0);
 
 		yinicial= espacio * l ;
 		xfinal = espacio * (l+1) ;
@@ -43,9 +46,10 @@ function dibujoPorClick() {
 
 }
 
+var color = "black";
 
-dibujarLinea ("black", 0, 0, 0, 300);
-dibujarLinea ("black", 0, 300, 300, 300);
-dibujarLinea ("black", 300, 0, 300, 300);
-dibujarLinea ("black", 0, 0, 300, 0);
+dibujarLinea (color, 0, 0, 0, 300);
+dibujarLinea (color,0, 300, 300, 300);
+dibujarLinea (color,300, 0, 300, 300);
+dibujarLinea (color,0, 0, 300, 0);
 
